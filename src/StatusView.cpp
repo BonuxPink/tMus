e06fca63438bff816ecb50efddfb7da16c2daf8b
BonuxPink <hackerdeez@protonmail.com>
2023-11-11 07:16:26 +0200

    Add StatusView

---
+++ src/StatusView.cpp
@@ -0,0 +1,106 @@
+/*
+ * Copyright (C) 2023 Dāniels Ponamarjovs <bonux@duck.com>
+ *
+ * This file is part of tMus.
+ *
+ * tMus is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * tMus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with tMus. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "StatusView.hpp"
+#include "Colors.hpp"
+#include "Renderer.hpp"
+
+#include <fmt/format.h>
+#include <ncpp/Utilities.hh>
+
+StatusView::StatusView(std::shared_ptr<AVFormatContext> formatCtx,
+                       std::shared_ptr<AVCodecContext> avctx,
+                       constructor_tag)
+    : m_formatCtx(std::move(formatCtx))
+    , m_avctx(std::move(avctx))
+{ }
+
+void StatusView::Create(std::shared_ptr<AVFormatContext> formatCtx, std::shared_ptr<AVCodecContext> avCodecCtx)
+{
+    instance = std::make_unique<StatusView>(formatCtx, avCodecCtx, constructor_tag{});
+    instance->m_ncp = Globals::statusPlane;
+}
+
+bool StatusView::handle_input([[maybe_unused]] const ncinput& ni) noexcept
+{
+    return false;
+}
+
+void StatusView::draw(std::size_t time)
+{
+    std::scoped_lock lk{ instance->mtx };
+    instance->internal_draw(time);
+}
+
+void StatusView::internal_draw(std::size_t time)
+{
+    m_ncp->get_dim(m_dimy, m_dimx);
+    m_ncp->set_channels((std::uint64_t)ncchannels_bchannel(Colors::StatusTextColor) << 32u | ncchannels_fchannel(Colors::StatusTextColor));
+    m_ncp->cursor_move((int)m_dimy - 1, 0);
+
+    ncpp::Cell transchar{};
+    m_ncp->hline(transchar, m_dimx - 1);
+
+    m_ncp->cursor_move((int)m_dimy - 1, 0);
+
+    auto secondsToTime = [](int seconds)
+    {
+        int hours = seconds / 360;
+        int minutes = (seconds / 60) % 60;
+        int secs = seconds % 60;
+
+        if (hours < 1)
+        {
+            return fmt::format("{:02}:{:02}", minutes, secs);
+        }
+        else
+        {
+            return fmt::format("{:02}:{:02}:{:02}", hours, minutes, secs);
+        }
+    };
+
+    const auto bytesPerSecond = 176400;
+    const auto seconds        = static_cast<int>(time) / bytesPerSecond;
+
+    const auto durationStr = secondsToTime(static_cast<int>(m_formatCtx->duration / AV_TIME_BASE));
+    const auto currentSecondStr = secondsToTime(static_cast<int>(seconds));
+
+    std::string_view url{ m_formatCtx->url };
+    auto pos = url.find_last_of('/');
+
+    std::string filename = fmt::format("{} > {} / {} {}", m_formatCtx->url, currentSecondStr, durationStr, std::floor(seconds));
+    filename.erase(0, pos + 1);
+
+    const auto* cStr = filename.c_str();
+
+    std::size_t sizeInBytes{ 0 };
+    while (*cStr)
+    {
+        int cols = ncplane_putegc_yx(m_ncp->to_ncplane(), -1, -1, cStr, &sizeInBytes);
+        cStr += sizeInBytes;
+
+        if (cols < 0)
+            break;
+
+        if (sizeInBytes == 0)
+            break;
+    }
+
+    Renderer::Render();
+}
---
+++ src/StatusView.hpp
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2023 Dāniels Ponamarjovs <bonux@duck.com>
+ *
+ * This file is part of tMus.
+ *
+ * tMus is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * tMus is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with tMus. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+extern "C"
+{
+    #include <libavformat/avformat.h>
+    #include <libavcodec/avcodec.h>
+}
+
+#include <ncpp/NotCurses.hh>
+#include <ncpp/Widget.hh>
+
+#include <memory>
+#include <thread>
+
+class StatusView
+{
+    struct constructor_tag { explicit constructor_tag() = default; };
+public:
+
+    StatusView(std::shared_ptr<AVFormatContext>,
+               std::shared_ptr<AVCodecContext>,
+               constructor_tag);
+
+    static std::unique_ptr<StatusView> instance;
+
+    static void Create(std::shared_ptr<AVFormatContext>,
+                std::shared_ptr<AVCodecContext>);
+
+    static void draw(std::size_t override = 0);
+    static void DestroyStatusPlane();
+    bool handle_input(const ncinput&) noexcept;
+private:
+
+    void internal_draw(std::size_t override);
+
+    mutable std::mutex mtx;
+    ncpp::Plane* m_ncp{};
+    std::shared_ptr<AVFormatContext> m_formatCtx;
+    std::shared_ptr<AVCodecContext> m_avctx;
+
+    unsigned m_dimy{};
+    unsigned m_dimx{};
+};
+
+inline std::unique_ptr<StatusView> StatusView::instance;
